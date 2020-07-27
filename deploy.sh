ad188d25484342f82575fb4a8636768142157a45
Oren Shadmi <OrenShadmi@gmail.com>
2020-07-27 17:32:22 +0300

    Update travis file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest  -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t orenshadmi/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t orenshadmi/multi-server:latest  -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t orenshadmi/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push orenshadmi/multi-client:latest
+docker push orenshadmi/multi-server:latest
+docker push orenshadmi/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push orenshadmi/multi-client:$SHA
+docker push orenshadmi/multi-server:$SHA
+docker push orenshadmi/multi-worker:$SHA
 
 kubectl apply -f k8s
 
